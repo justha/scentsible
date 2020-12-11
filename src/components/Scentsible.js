130e40507525fa6aa7eb3688e6c153f69d9fc595
justha <nganha42@gmail.com>
2020-12-11 15:31:37 -0600

    initial commit; react-app set up

---
+++ src/components/Scentsible.js
@@ -0,0 +1,9 @@
+import { React } from "react"
+import "./Scentsible.css"
+
+export const Scentsible = () => (
+    <>
+        <h2>Home</h2>
+        <div>Welcome scentsible consumer</div>
+    </>
+)
