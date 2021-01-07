b079941b41f8011d28053b8518af13aceb45c4b3
justha <nganha42@gmail.com>
2021-01-07 01:43:47 -0600

    providers for product, productreview

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,6 +6,11 @@ import { GroupProvider } from "./group/GroupProvider"
 import { RatingProvider } from "./rating/RatingProvider"
 import { ProductProvider } from "./product/ProductProvider"
 import { ProductForm } from "./product/ProductForm"
+import { ProductList } from "./product/ProductList"
+import { ProductDetail } from "./product/ProductDetail"
+import { ProductreviewProvider } from "./productreview/ProductreviewProvider"
+import { ProductreviewForm } from "./productreview/ProductreviewForm"
+import { ProductreviewList } from "./productreview/ProductreviewList"
 
 
 export const ApplicationViews = (props) => {
@@ -14,28 +19,52 @@ export const ApplicationViews = (props) => {
             <Route exact path="/">
             </Route>
 
-            {/* <ProductProvider>
-                <BrandProvider>
-                    <FamilyProvider>
-                        <GroupProvider>
-                            <RatingProvider>
-                                <Route exact path="/products/create" 
-                                    render={(props) => <ProductForm {...props} />}
-                                />
-                                <Route exact path="/products/edit/:productId(\d+)" 
-                                    render={(props) => <ProductForm {...props} />}
-                                />
-                                <Route exact path="/myproducts" 
-                                    render={(props) => <UsersProducts {...props} />}
-                                />
-                                <Route path="/products/:productId(\d+)"
-                                    render={(props) => <ProductDetails {...props} />}
-                                />                                
-                            </RatingProvider>
-                        </GroupProvider>
-                    </FamilyProvider>
-                </BrandProvider>
-            </ProductProvider> */}
+            <ProductProvider>
+                {/* <ProductreviewProvider> */}
+                    <BrandProvider>
+                        <FamilyProvider>
+                            <GroupProvider>
+                                <RatingProvider>
+
+                                    <Route exact path="/products" 
+                                        render={(props) => <ProductList {...props} />}
+                                    />
+
+                                    {/* <Route exact path="/products/create" 
+                                        render={(props) => <ProductForm {...props} />}
+                                    /> */}
+
+                                    {/* <Route exact path="/products/edit/:productId(\d+)" 
+                                        render={(props) => <ProductForm {...props} />}
+                                    /> */}
+                                    
+                                    {/* <Route path="/products/:productId(\d+)"
+                                        render={(props) => <ProductDetail {...props} />}
+                                    /> */}
+                                    
+                                </RatingProvider>
+                            </GroupProvider>
+                        </FamilyProvider>
+                    </BrandProvider>
+                {/* </ProductreviewProvider> */}
+            </ProductProvider>
+
+
+            {/* <ProductreviewProvider>
+                <ProductProvider>
+                    <RatingProvider>
+
+                        <Route exact path="/productreviews" 
+                            render={(props) => <ProductreviewList {...props} />}
+                        />
+                        
+                        <Route exact path="/productreviews/create" 
+                            render={(props) => <ProductreviewForm {...props} />}
+                        />                        
+                        
+                    </RatingProvider>
+                </ProductProvider>                
+            </ProductreviewProvider> */}
 
         </>
     )
---
+++ src/components/productreview/ProductreviewList.js
@@ -0,0 +1,2 @@
+
+export const ProductreviewList = (props) => {}
