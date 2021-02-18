497bb969d73115a4c6d3f43c56669e9036bd2002
justha <nganha42@gmail.com>
2021-02-18 10:24:50 -0600

    logo click routes to new homepage

--- src/components/home/Homepage.js
+++ src/components/home/Homepage.js
@@ -1,11 +1,29 @@
 import React, { useContext, useEffect, useState} from "react"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
+import { GroupContext } from "../group/GroupProvider"
+import Button from '@material-ui/core/Button'
 
 
 export const Homepage = ({ props }) => {
+    const { groups, getGroups, selectedGroupId, setSelectedGroupId } = useContext(GroupContext)    
+    const history = useHistory()
+
+
     return(
         <>
-            <div></div>
+            <div className="container__homepageButtons">
+                <Button 
+                    className="button--selectGroup" 
+                    as={Link} 
+                    onClick={() => {history.push({ pathname: "/products"})}}
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    // startIcon={<AddIcon />}
+                    >
+                    Hair
+                </Button>                
+            </div>
         </>
     )
 }
